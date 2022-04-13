0a1a1bf3b819e83d5d5b5fd0870284e5a691ab45
Nortum <varevtsev@yandex.ru>
2022-04-13 17:05:13 +0400

    1

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -8,6 +8,7 @@ public class User {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "user_id")
    private Long id;
 
    @Column(name = "name")
@@ -19,8 +20,12 @@ public class User {
    @Column(name = "email")
    private String email;
 
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name = "users_id")
+   private Car car = new Car();
+
    public User() {}
-   
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
@@ -30,32 +35,42 @@ public class User {
    public Long getId() {
       return id;
    }
-
    public void setId(Long id) {
       this.id = id;
    }
-
    public String getFirstName() {
       return firstName;
    }
-
    public void setFirstName(String firstName) {
       this.firstName = firstName;
    }
-
    public String getLastName() {
       return lastName;
    }
-
    public void setLastName(String lastName) {
       this.lastName = lastName;
    }
-
    public String getEmail() {
       return email;
    }
-
    public void setEmail(String email) {
       this.email = email;
+   }
+   public Car getCar() {
+      return car;
+   }
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,9 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Transactional(readOnly = true)
+   @Override
+   public User userOfCar(String model, int series) {
+      return userDao.userOfCar(model, series);
+   }
 }
